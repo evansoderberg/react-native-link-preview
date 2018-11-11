50bce9b09a072e5c3bb0c3342e967fc218b36215
Evan Soderberg <evansoderberg@gmail.com>
2018-11-12 10:10:06 +1100

    change promise reject to console warns

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 /**
-* @providesModule react-native-link-preview
-*/
+ * @providesModule react-native-link-preview
+ */
 
-const cheerio = require('cheerio-without-node-native');
-const urlObj = require('url');
-const fetch = require('cross-fetch').fetch;
-require('es6-promise').polyfill();
+const cheerio = require("cheerio-without-node-native");
+const urlObj = require("url");
+const fetch = require("cross-fetch").fetch;
+require("es6-promise").polyfill();
 
-const CONSTANTS = require('./constants');
+const CONSTANTS = require("./constants");
 
 exports.getPreview = function(text, options) {
   return new Promise(function(resolve, reject) {
     if (!text) {
-      reject({
-        error: 'React-Native-Link-Preview did not receive either a url or text'
-      });
+      console.warn(
+        "React-Native-Link-Preview did not receive either a url or text"
+      );
     }
 
     var detectedUrl = null;
 
-    text.replace(/\n/g, ' ').split(' ').forEach(function(token) {
-      if (CONSTANTS.REGEX_VALID_URL.test(token) && !detectedUrl) {
-        detectedUrl = token;
-      }
-    });
+    text
+      .replace(/\n/g, " ")
+      .split(" ")
+      .forEach(function(token) {
+        if (CONSTANTS.REGEX_VALID_URL.test(token) && !detectedUrl) {
+          detectedUrl = token;
+        }
+      });
 
     if (detectedUrl) {
       fetch(detectedUrl)
@@ -32,44 +35,65 @@ exports.getPreview = function(text, options) {
           const finalUrl = response.url;
 
           // get content type of response
-          var contentType = findById(response.headers, 'content-type');
+          var contentType = findById(response.headers, "content-type");
           if (!contentType) {
-            return reject({ error: 'React-Native-Link-Preview: Could not extract content type for URL.' });
+            console.warn(
+              "React-Native-Link-Preview: Could not extract content type for URL."
+            );
+            return;
           }
           if (contentType instanceof Array) {
             contentType = contentType[0];
           }
 
           // parse response depending on content type
-          if (contentType && CONSTANTS.REGEX_CONTENT_TYPE_IMAGE.test(contentType)) {
+          if (
+            contentType &&
+            CONSTANTS.REGEX_CONTENT_TYPE_IMAGE.test(contentType)
+          ) {
             resolve(parseImageResponse(finalUrl, contentType));
-          } else if (contentType && CONSTANTS.REGEX_CONTENT_TYPE_AUDIO.test(contentType)) {
+          } else if (
+            contentType &&
+            CONSTANTS.REGEX_CONTENT_TYPE_AUDIO.test(contentType)
+          ) {
             resolve(parseAudioResponse(finalUrl, contentType));
-          } else if (contentType && CONSTANTS.REGEX_CONTENT_TYPE_VIDEO.test(contentType)) {
+          } else if (
+            contentType &&
+            CONSTANTS.REGEX_CONTENT_TYPE_VIDEO.test(contentType)
+          ) {
             resolve(parseVideoResponse(finalUrl, contentType));
-          } else if (contentType && CONSTANTS.REGEX_CONTENT_TYPE_TEXT.test(contentType)) {
-            response.text()
-              .then(function(text) {
-                resolve(parseTextResponse(text, finalUrl, options || {}, contentType));
-              });
-          } else if (contentType && CONSTANTS.REGEX_CONTENT_TYPE_APPLICATION.test(contentType)) {
+          } else if (
+            contentType &&
+            CONSTANTS.REGEX_CONTENT_TYPE_TEXT.test(contentType)
+          ) {
+            response.text().then(function(text) {
+              resolve(
+                parseTextResponse(text, finalUrl, options || {}, contentType)
+              );
+            });
+          } else if (
+            contentType &&
+            CONSTANTS.REGEX_CONTENT_TYPE_APPLICATION.test(contentType)
+          ) {
             resolve(parseApplicationResponse(finalUrl, contentType));
           } else {
-            reject({ error: 'React-Native-Link-Preview: Unknown content type for URL.' });
+            console.warn(
+              "React-Native-Link-Preview: Unknown content type for URL."
+            );
           }
         })
-        .catch(function(error) { reject({ error: error }) });
+        .catch(function(error) {
+          console.warn(error);
+        });
     } else {
-      reject({
-        error: 'React-Native-Link-Preview did not find a link in the text'
-      });
+      console.warn("React-Native-Link-Preview did not find a link in the text");
     }
   });
 };
 
 // recursively search through object to find property with given id
 // returns value if found, undefined if not found
-const findById = function (object, key) {
+const findById = function(object, key) {
   var value;
 
   Object.keys(object).some(function(k) {
@@ -77,7 +101,7 @@ const findById = function (object, key) {
       value = object[k];
       return true;
     }
-    if (object[k] && typeof object[k] === 'object') {
+    if (object[k] && typeof object[k] === "object") {
       value = findById(object[k], key);
       return value !== undefined;
     }
@@ -86,50 +110,50 @@ const findById = function (object, key) {
   return value;
 };
 
-const parseImageResponse = function (url, contentType) {
+const parseImageResponse = function(url, contentType) {
   return {
     url: url,
-    mediaType: 'image',
+    mediaType: "image",
     contentType: contentType,
     favicons: [getDefaultFavicon(url)]
   };
 };
 
-const parseAudioResponse = function (url, contentType) {
+const parseAudioResponse = function(url, contentType) {
   return {
     url: url,
-    mediaType: 'audio',
+    mediaType: "audio",
     contentType: contentType,
     favicons: [getDefaultFavicon(url)]
   };
 };
 
-const parseVideoResponse = function (url, contentType) {
+const parseVideoResponse = function(url, contentType) {
   return {
     url: url,
-    mediaType: 'video',
+    mediaType: "video",
     contentType: contentType,
     favicons: [getDefaultFavicon(url)]
   };
 };
 
-const parseApplicationResponse = function (url, contentType) {
+const parseApplicationResponse = function(url, contentType) {
   return {
     url: url,
-    mediaType: 'application',
+    mediaType: "application",
     contentType: contentType,
     favicons: [getDefaultFavicon(url)]
   };
 };
 
-const parseTextResponse = function (body, url, options, contentType) {
+const parseTextResponse = function(body, url, options, contentType) {
   const doc = cheerio.load(body);
 
   return {
     url: url,
     title: getTitle(doc),
     description: getDescription(doc),
-    mediaType: getMediaType(doc) || 'website',
+    mediaType: getMediaType(doc) || "website",
     contentType: contentType,
     images: getImages(doc, url, options.imagesPropertyType),
     videos: getVideos(doc),
@@ -138,37 +162,37 @@ const parseTextResponse = function (body, url, options, contentType) {
 };
 
 const getTitle = function(doc) {
-  var title = doc("meta[property='og:title']").attr('content');
+  var title = doc("meta[property='og:title']").attr("content");
 
   if (!title) {
-    title = doc('title').text();
+    title = doc("title").text();
   }
 
   return title;
 };
 
 const getDescription = function(doc) {
-  var description = doc('meta[name=description]').attr('content');
+  var description = doc("meta[name=description]").attr("content");
 
   if (description === undefined) {
-    description = doc('meta[name=Description]').attr('content');
+    description = doc("meta[name=Description]").attr("content");
   }
 
   if (description === undefined) {
-    description = doc("meta[property='og:description']").attr('content');
+    description = doc("meta[property='og:description']").attr("content");
   }
 
   return description;
 };
 
 const getMediaType = function(doc) {
-  const node = doc('meta[name=medium]');
+  const node = doc("meta[name=medium]");
 
   if (node.length) {
-    const content = node.attr('content');
-    return content === 'image' ? 'photo' : content;
+    const content = node.attr("content");
+    return content === "image" ? "photo" : content;
   } else {
-    return doc("meta[property='og:type']").attr('content');
+    return doc("meta[property='og:type']").attr("content");
   }
 };
 
@@ -178,8 +202,8 @@ const getImages = function(doc, rootUrl, imagesPropertyType) {
     src,
     dic;
 
-  var imagePropertyType = imagesPropertyType || 'og'
-  nodes = doc('meta[property=\'' + imagePropertyType + ':image\']');
+  var imagePropertyType = imagesPropertyType || "og";
+  nodes = doc("meta[property='" + imagePropertyType + ":image']");
 
   if (nodes.length) {
     nodes.each(function(index, node) {
@@ -192,12 +216,12 @@ const getImages = function(doc, rootUrl, imagesPropertyType) {
   }
 
   if (images.length <= 0 && !imagesPropertyType) {
-    src = doc('link[rel=image_src]').attr('href');
+    src = doc("link[rel=image_src]").attr("href");
     if (src) {
       src = urlObj.resolve(rootUrl, src);
       images = [src];
     } else {
-      nodes = doc('img');
+      nodes = doc("img");
 
       if (nodes.length) {
         dic = {};
@@ -238,8 +262,8 @@ const getVideos = function(doc) {
   if (length) {
     nodeTypes = doc("meta[property='og:video:type']");
     nodeSecureUrls = doc("meta[property='og:video:secure_url']");
-    width = doc("meta[property='og:video:width']").attr('content');
-    height = doc("meta[property='og:video:height']").attr('content');
+    width = doc("meta[property='og:video:width']").attr("content");
+    height = doc("meta[property='og:video:height']").attr("content");
 
     for (index = 0; index < length; index++) {
       video = nodes[index].attribs.content;
@@ -257,7 +281,7 @@ const getVideos = function(doc) {
         width: width,
         height: width
       };
-      if (videoType.indexOf('video/') === 0) {
+      if (videoType.indexOf("video/") === 0) {
         videos.splice(0, 0, videoObj);
       } else {
         videos.push(videoObj);
@@ -274,11 +298,15 @@ const getFavicons = function(doc, rootUrl) {
     nodes = [],
     src;
 
-  const relSelectors = ['rel=icon', 'rel="shortcut icon"', 'rel=apple-touch-icon'];
+  const relSelectors = [
+    "rel=icon",
+    'rel="shortcut icon"',
+    "rel=apple-touch-icon"
+  ];
 
   relSelectors.forEach(function(relSelector) {
     // look for all icon tags
-    nodes = doc('link[' + relSelector + ']');
+    nodes = doc("link[" + relSelector + "]");
 
     // collect all images from icon tags
     if (nodes.length) {
@@ -301,6 +329,6 @@ const getFavicons = function(doc, rootUrl) {
 };
 
 // returns default favicon (//hostname/favicon.ico) for a url
-const getDefaultFavicon = function (rootUrl) {
-  return urlObj.resolve(rootUrl, '/favicon.ico');
+const getDefaultFavicon = function(rootUrl) {
+  return urlObj.resolve(rootUrl, "/favicon.ico");
 };
